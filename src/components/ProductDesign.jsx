c8ceeb7a34a57994c0e24e6965053a5b76dba152
lekrikri <ganou.christophe@gmail.com>
2023-06-19 10:18:06 +0200

    Update ProductDesign.jsx

--- src/components/ProductDesign.jsx
+++ src/components/ProductDesign.jsx
@@ -1,26 +1,26 @@
-import { OrbitControls, PresentationControls, Stage } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
-import Drone from './Drone'
+import { OrbitControls, PresentationControls, Stage } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import Drone from "./Drone";
 
 const ProductDesign = () => {
   return (
     <Canvas>
-      <Stage environment="city" intensity={0.6}>
-      <PresentationControls
-        global
-        rotation={[0, 0.6, 0]}
-        polar={[0.85, 0.2]} //verticale
-        azimuth={[-1, 0.75]} //horizontale
-        config={{ mass: 2, tension: 400 }} //glisser-deposer
-        // snap={{ mass: 2, tension: 300 }} //liberation
-      >
-        <Drone/>
-      </PresentationControls>
+      <Stage environment="studio" intensity={0.6}>
+        <PresentationControls
+          global
+          rotation={[0, 0.6, 0]}
+          polar={[0.85, 0.2]} //verticale
+          azimuth={[-1, 0.75]} //horizontale
+          config={{ mass: 2, tension: 400 }} //glisser-deposer
+          // snap={{ mass: 2, tension: 300 }} //liberation
+        >
+          <Drone />
+        </PresentationControls>
       </Stage>
-      <OrbitControls enableZoom={true}/>
+      <OrbitControls enableZoom={true} />
     </Canvas>
-  )
-}
+  );
+};
 
-export default ProductDesign
+export default ProductDesign;
