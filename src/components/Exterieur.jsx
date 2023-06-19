73e0a202a77ca15f1bf3b89583e58c45e5b5df79
lekrikri <ganou.christophe@gmail.com>
2023-06-19 10:15:01 +0200

    Mise a jour

--- src/components/Exterieur.jsx
+++ src/components/Exterieur.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   ContactShadows,
   PresentationControls,
@@ -6,32 +6,30 @@ import {
   OrbitControls,
   Stage,
 } from "@react-three/drei";
-import { Canvas } from '@react-three/fiber';
-import House from './House';
-
+import { Canvas } from "@react-three/fiber";
+import House from "./House";
 
 const Exterieur = () => {
-
   return (
     <Canvas>
-      <Stage environment="park" intensity={0.45}>
-      <PresentationControls
-        global
-        rotation={[0, -3.6, 0]}
-        polar={[-0.4, 0.2]} //verticale
-        azimuth={[-1, 0.75]} //horizontale
-        config={{ mass: 2, tension: 400 }} //glisser-deposer
-        snap={{ mass: 2, tension: 300 }} //liberation
-      >
-        <Float rotationIntensity={0.55}>
-          <House/>
-        </Float>
-      </PresentationControls>
-      <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
+      <Stage environment="city" intensity={0.45}>
+        <PresentationControls
+          global
+          rotation={[0, -3.6, 0]}
+          polar={[-0.4, 0.2]} //verticale
+          azimuth={[-1, 0.75]} //horizontale
+          config={{ mass: 2, tension: 400 }} //glisser-deposer
+          snap={{ mass: 2, tension: 300 }} //liberation
+        >
+          <Float rotationIntensity={0.55}>
+            <House />
+          </Float>
+        </PresentationControls>
+        <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
       </Stage>
-      <OrbitControls enableZoom={true}/>
+      <OrbitControls enableZoom={true} />
     </Canvas>
-  )
-}
+  );
+};
 
-export default Exterieur
+export default Exterieur;
--- src/components/Interieur.jsx
+++ src/components/Interieur.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Html,
   ContactShadows,
@@ -8,31 +8,30 @@ import {
   OrbitControls,
   Stage,
 } from "@react-three/drei";
-import { Canvas } from '@react-three/fiber';
-import HomeInterieur from './HomeInterieur';
+import { Canvas } from "@react-three/fiber";
+import HomeInterieur from "./HomeInterieur";
 
 const Interieur = () => {
-
   return (
     <Canvas>
-      <Stage environment="park" intensity={0.65}>
-      <PresentationControls
-        global
-        rotation={[0, 0.6, 0]}
-        polar={[0.55, 0.2]} //verticale
-        azimuth={[-1, 0.75]} //horizontale
-        config={{ mass: 2, tension: 400 }} //glisser-deposer
-        snap={{ mass: 2, tension: 300 }} //liberation
-      >
-        <Float rotationIntensity={0.55}>
-          <HomeInterieur/>
-        </Float>
-      </PresentationControls>
-      <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
+      <Stage environment="city" intensity={0.65}>
+        <PresentationControls
+          global
+          rotation={[0, 0.6, 0]}
+          polar={[0.55, 0.2]} //verticale
+          azimuth={[-1, 0.75]} //horizontale
+          config={{ mass: 2, tension: 400 }} //glisser-deposer
+          snap={{ mass: 2, tension: 300 }} //liberation
+        >
+          <Float rotationIntensity={0.55}>
+            <HomeInterieur />
+          </Float>
+        </PresentationControls>
+        <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
       </Stage>
-      <OrbitControls enableZoom={true}/>
+      <OrbitControls enableZoom={true} />
     </Canvas>
-  )
-}
+  );
+};
 
-export default Interieur
+export default Interieur;
